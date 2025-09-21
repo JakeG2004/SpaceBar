ca68ff0939487d157b31f7f3d38ddfc41f27d6ec
Jake Gendreau <jake.lgendreau@gmail.com>
2025-09-21 13:01:33 -0700

    Moslty finished with dish washing minigame

--- Assets/Scripts/Minigames/Dishwashing/DirtyCup.cs
+++ Assets/Scripts/Minigames/Dishwashing/DirtyCup.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DirtyCup : MonoBehaviour
 {
+    [SerializeField] private float _speed = 1f;
     [SerializeField] private int _targetCleanliness = 10;
     [SerializeField] private Vector2 _bubbleRange = new Vector2(1, 3);
     [SerializeField] private GameObject _bubble;
     [SerializeField] private Transform _bubbleSpawnPoint;
+
+    [Header("Colors")]
+    [SerializeField] private Color _dirtyColor;
+    [SerializeField] private Color _cleanColor;
+
+    [SerializeField] private VoidEventChannelSO _cupCleanEvent;
+    [SerializeField] private UnityEvent _onClean;
+
+    private DishwashingMinigame _minigame;
 
     private bool _isClean = false;
     private int _cleanliness = 0;
@@ -18,29 +29,58 @@ public class DirtyCup : MonoBehaviour
         {
             _renderer = GetComponent<SpriteRenderer>();
         }
+
+        _renderer.color = _dirtyColor;
     }
 
     void Update()
     {
+        Vector3 newPos = transform.position;
+        newPos.x -= (_speed * Time.deltaTime);
+        if(newPos.x < -12)
+        {
+            newPos.x = 12;
 
+            if(_isClean)
+            {
+                _cupCleanEvent.RaiseEvent();
+                if(_minigame.numDirtyCups >= 0)
+                {
+                    _isClean = false;
+                    _renderer.color = _dirtyColor;
+                }
+
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+        transform.position = newPos;
     }
     
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Test");
         if(other.CompareTag("Water"))
         {
+            Destroy(other.gameObject);
             SpawnBubbles();
             _cleanliness++;
 
             // Change cup color to be more clear
-            _renderer.color = Color.Lerp(_renderer.color, Color.white, (float)_cleanliness / _targetCleanliness);
+            _renderer.color = Color.Lerp(_dirtyColor, _cleanColor, (float)_cleanliness / _targetCleanliness);
 
             if(_cleanliness >= _targetCleanliness)
             {
+                _onClean?.Invoke();
                 _isClean = true;
             }
         }
+    }
+
+    public void SetMinigame(DishwashingMinigame dish)
+    {
+        _minigame = dish;
     }
 
     private void SpawnBubbles()
@@ -53,15 +93,20 @@ public class DirtyCup : MonoBehaviour
             tmpSpawn.x += Random.Range(-2.0f, 2.0f);
             tmpSpawn.y += Random.Range(-2.0f, 2.0f);
 
-            Object.Instantiate(_bubble, tmpSpawn, Quaternion.Euler(0, 0, Random.Range(0f, 360f)));
-        }
-    }
+            GameObject bubble = Object.Instantiate(
+                _bubble,
+                tmpSpawn,
+                Quaternion.Euler(0, 0, Random.Range(0f, 360f)),
+                transform // keep parent
+            );
 
-    private void ResetPos()
-    {
-        if(_isClean)
-        {
-            Destroy(this.gameObject);
+            // Convert local scale so that global scale = 1
+            Vector3 parentScale = transform.lossyScale;
+            bubble.transform.localScale = new Vector3(
+                1f / parentScale.x,
+                1f / parentScale.y,
+                1f / parentScale.z
+            );
         }
     }
 }
--- Assets/Scripts/Minigames/Dishwashing/DishwashingMinigame.cs
+++ Assets/Scripts/Minigames/Dishwashing/DishwashingMinigame.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class DishwashingMinigame : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject _waterPrefab;
-    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform _waterSpawnPoint;
+    [SerializeField] private GameObject _cupPrefab;
+    [SerializeField] private Transform _cupSpawnPoint;
 
     [Header("Settings")]
     [SerializeField] private float _maxPressure = 1f;        // Max faucet pressure
     [SerializeField] private float _pressureRechargeRate = 0.2f; // Pressure restored per second
     [SerializeField] private float _pressureDrainPerWater = 0.05f; // Pressure lost per water unit
     [SerializeField] private int _maxWaterPerDispense = 10;  // Max number of water prefabs per use
+    [SerializeField] private float _cupSpawnTime = 3f;
+
+    [Header("Cup Variables")]
+    public int numDirtyCups = 0;
+    public int numCleanCups = 0;
+    [SerializeField] private int _numCupsToClean = 0;
+
+    [Header("End minigame variables")]
+    [SerializeField] private GameObject _winText;
+    [SerializeField] private UnityEvent _winEvent;
 
     private float _waterPressure;
     private bool _isDispensing = false;
 
     void OnEnable()
     {
+        _winText.SetActive(false);
+        numDirtyCups = DrinkManager.Instance.dirtyCups;
+        _numCupsToClean = numDirtyCups;
         _waterPressure = _maxPressure; // start full
         StartCoroutine(RegeneratePressure());
+        StartCoroutine(SpawnCups());
     }
 
     void OnDisable()
@@ -34,7 +51,20 @@ public class DishwashingMinigame : MonoBehaviour
         // Calculate how many water drops we can spawn based on current pressure
         int numWater = Mathf.RoundToInt(_maxWaterPerDispense * _waterPressure);
         if (numWater > 0)
+        {
             StartCoroutine(DispenseWater(numWater));
+        }
+    }
+
+    public void CupCleaned()
+    {
+        numDirtyCups--;
+        numCleanCups++;
+
+        if(numCleanCups >= _numCupsToClean)
+        {
+            StartCoroutine(WinGame());
+        }
     }
 
     private IEnumerator RegeneratePressure()
@@ -55,10 +85,35 @@ public class DishwashingMinigame : MonoBehaviour
         _isDispensing = true;
         for (int i = 0; i < amount; i++)
         {
-            Instantiate(_waterPrefab, _spawnPoint.position, Quaternion.identity);
+            Instantiate(_waterPrefab, _waterSpawnPoint.position, Quaternion.identity);
             _waterPressure = Mathf.Clamp01(_waterPressure - _pressureDrainPerWater);
             yield return new WaitForSeconds(0.05f);
         }
         _isDispensing = false;
+    }
+
+    private IEnumerator SpawnCups()
+    {
+        int cupsToSpawn = numDirtyCups;
+        if(numDirtyCups > 4)
+        {
+            cupsToSpawn = 4;
+        }
+
+        for(int i = 0; i < cupsToSpawn; i++)
+        {
+            Instantiate(_cupPrefab, _cupSpawnPoint).GetComponent<DirtyCup>().SetMinigame(this);
+            numDirtyCups--;
+
+            yield return new WaitForSeconds(_cupSpawnTime);
+        }
+    }
+
+    private IEnumerator WinGame()
+    {
+        _winText.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+        _winEvent?.Invoke();
     }
 }
