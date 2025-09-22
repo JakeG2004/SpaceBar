2938bf6c53561aebe4d702588b1ce80e59f3de64
Michael Crapse <mikeeybikeey@gmail.com>
2025-09-21 18:55:03 -0700

    Add game timer

--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -1,8 +1,37 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+    public int DrinksServed { get; private set; }
+    private float _gameTimer = 60f * 4f;
+    public VoidEventChannelSO TimerExpiredEvent;
+
+    void Awake()
+    {
+        if(Instance != this && Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Update()
+    {
+        _gameTimer = Math.Max(_gameTimer - Time.deltaTime, 0f);
+        if (_gameTimer == 0f)
+        {
+            TimerExpiredEvent.RaiseEvent();
+        }
+    }
+
+    public float GetGameTimer() => _gameTimer;
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
